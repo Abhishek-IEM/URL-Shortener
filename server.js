40ce65cec047e3d86d49c6bce4504a9a13e46109
Abhishek-IEM <as6119552@gmail.com>
2025-01-13 21:42:01 +0530

    URL Shortener

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import { shortUrl, getOriginalUrl } from "./controllers/url.js";
+import { config } from "dotenv";
 
 const app = express();
+
+config({ path: ".env" });
 
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
-  .connect(
-    "mongodb+srv://as6119552:09VIuGVDYpCIUkyh@cluster0.fa4q9.mongodb.net/",
-    {
-      dbName: "URL_Shortener",
-    }
-  )
+  .connect(process.env.MONGO_URI, {
+    dbName: "URL_Shortener",
+  })
   .then(() => console.log("MongoDb Connected..!"))
   .catch((err) => console.log(err));
 
@@ -27,7 +27,7 @@ app.post("/short", shortUrl);
 // redirect to original url using short code :- dynamic routing
 app.get("/:shortCode", getOriginalUrl);
 
-const port = 1000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
